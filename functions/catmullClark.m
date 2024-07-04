f61d11c619b292f19319664ece7ab0deec0be90e
rohan-collins <rohan.chabukswar@collins.com>
2024-07-04 16:32:56 +0100

    Adding function for recursive subdivision surface generation using Catmull-Clark algorithm.

---
+++ functions/catmullClark.m
@@ -0,0 +1,221 @@
+function [F,V,varargout]=catmullClark(F,V,varargin)
+    % Create a subdivision surface by refining the given mesh using the
+    % Catmull-Clark algorithm.
+    %
+    % [F,V]=CATMULLCLARK(F,V) subdivides the given mesh by one level using
+    % the Catmull-Clark algorithm. F will be returned as quadrilaterals,
+    % which in general will not be planar, as is default for the
+    % Catmull-Clark algorithm.
+    %
+    % [...]=CATMULLCLARK(...,'level',LEVELS) subdivides the given mesh
+    % recursively LEVELS times.
+    %
+    % [...,UV]=CATMULLCLARK(...,'TEXCOORD',UV) subdivides the given UV
+    % coordinates along with the vertices, while handling vertices with
+    % same XYZ coordinates but different UV coordinates.
+    %
+    % [...,J,W]=CATMULLCLARK(...,'JOINTS',J,'WEIGHTS',W) subdivides the
+    % mesh and returns the influence of the original vertices as joints and
+    % weights for skinned animation.
+    %
+    % © Copyright 2014-2024 Rohan Chabukswar.
+    %
+    % This file is part of MATLAB GLTF.
+    %
+    % MATLAB GLTF is free software: you can redistribute it and/or modify
+    % it under the terms of the GNU General Public License as published by
+    % the Free Software Foundation, either version 3 of the License, or (at
+    % your option) any later version.
+    %
+    % MATLAB GLTF is distributed in the hope that it will be useful, but
+    % WITHOUT ANY WARRANTY; without even the implied warranty of
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    % General Public License for more details.
+    %
+    % You should have received a copy of the GNU General Public License
+    % along with MATLAB GLTF. If not, see <https://www.gnu.org/licenses/>.
+    %
+    ips=inputParser;
+    ips.addParameter('TEXCOORD',[],@isnumeric);
+    ips.addParameter('JOINTS',[],@isnumeric);
+    ips.addParameter('WEIGHTS',[],@isnumeric);
+    ips.addParameter('level',1,@(x)GLTF.validateInteger(x,0,inf));
+    ips.parse(varargin{:});
+    parameters=ips.Results;
+    UV=parameters.TEXCOORD;
+    J=parameters.JOINTS;
+    W=parameters.WEIGHTS;
+    level=parameters.level;
+    if(and(~isempty(J),~isempty(W)))
+        JW=toJW(J,W);
+    else
+        JW=[];
+    end
+    if(nargin<5)
+        level=1;
+    end
+    if(nargin<4)
+        JW=[];
+    end
+    if(nargin<3)
+        UV=nan(size(V,1),0);
+    end
+    for l=1:level
+        F1=F;
+        if(isempty(UV))
+            V1=V;
+        else
+            [V1,ia,ic]=unique(V,"rows","stable");
+            VV1=speye(size(V1,1));
+            VV1=VV1(ic,:);
+            IA=false(size(V,1),1);
+            IA(ia)=true;
+            if(isempty(JW))
+                JW=eye(size(V,1));
+                JW=JW(IA,IA);
+            else
+                JW=JW(IA,:);
+            end
+            F1(~isnan(F1))=ic(F(~isnan(F)));
+        end
+        
+        F2=F;
+        F3=F1;
+        for i=1:size(F,1)
+            F2(i,~isnan(F(i,:)))=circshift(F(i,~isnan(F(i,:))),-1,2);
+            F3(i,~isnan(F1(i,:)))=circshift(F1(i,~isnan(F1(i,:))),-1,2);
+        end
+        E=reshape(permute(cat(3,F,F2),[3 2 1]),2,numel(F))';
+        E1=reshape(permute(cat(3,F1,F3),[3 2 1]),2,numel(F1))';
+        clear F3;
+        
+        E2=E(~any(isnan(E),2),:);
+        E=sort(E2,2);
+        clear E2;
+        [E,~,ic]=unique(E,"rows","stable");
+        E3=E1(~any(isnan(E1),2),:);
+        E1=sort(E3,2);
+        clear E3;
+        [E1,~,ic1]=unique(E1,"rows","stable");
+        EE1=sparse(ic,ic1,true,size(E,1),size(E1,1));
+        
+        dirF=F<F2;
+        clear F2;
+        
+        FE=F';
+        FE(~isnan(FE))=ic;
+        clear ic;
+        FE2=FE;
+        FE=sparse(cell2mat(cellfun(@(x,y)repmat(x,y,1),num2cell(1:size(F,1))',num2cell(sum(~isnan(F),2)),'UniformOutput',false)),FE2(~isnan(FE2)),true,size(F,1),size(E,1));
+        
+        F1E1=F1';
+        F1E1(~isnan(F1E1))=ic1;
+        clear ic1;
+        FE3=F1E1;
+        F1E1=sparse(cell2mat(cellfun(@(x,y)repmat(x,y,1),num2cell(1:size(F1,1))',num2cell(sum(~isnan(F1),2)),'UniformOutput',false)),FE3(~isnan(FE3)),true,size(F1,1),size(E1,1));
+        clear FE3;
+        
+        VE=sparse(E(:)',[1:size(E,1) 1:size(E,1)],true,size(V,1),size(E,1));
+        V1E1=sparse(E1(:)',[1:size(E1,1) 1:size(E1,1)],true,size(V1,1),size(E1,1));
+        
+        [I,J]=find(~isnan(F));
+        J=F(sub2ind(size(F),I,J));
+        VF=sparse(J,I,true,size(V,1),size(F,1));
+        clear I J;
+
+        [I,J]=find(~isnan(F1));
+        J=F1(sub2ind(size(F1),I,J));
+        V1F1=sparse(J,I,true,size(V1,1),size(F1,1));
+        clear I J;
+        
+        dir2=dirF'>0;
+        [~,J]=find(and(dirF'==1,~isnan(dirF')));
+        dirF=sparse(J,FE2(dir2),true,size(F,1),size(E,1));
+        clear dir2 J FE2;
+        
+        dirV=sparse(E(:,1),1:size(E,1),true,size(V,1),size(E,1));
+        clear E;
+        
+        temp=(V1F1./sum(V1F1));
+        FP=temp'*V1;
+        JW_F=temp'*JW;
+        JW_E=(V1E1./sum(V1E1))'*JW;
+        EP=(F1E1'*FP+V1E1'*V1)/4;
+        clear F1E1 temp;
+        EMP=V1E1'*V1/2;
+        FF=(V1F1./sum(V1F1,2))*FP;
+        R=V1E1./sum(V1E1,2)*EMP;
+        clear V1E1 EMP;
+        n=sum(V1F1,2);
+        clear V1F1;
+        VP=(FF+2*R+(n-3).*V1)./n;
+        clear FF R n V1;
+        if(isempty(UV))
+            newV=[VP;FP;EP];
+            newJW=[JW;JW_F;JW_E];
+        else
+            newV=[VV1*VP;FP;EE1*EP];
+            newJW=[VV1*JW;JW_F;EE1*JW_E];
+            clear VV1 EE1;
+        end
+        clear VP FP EP;
+
+        if(~isempty(UV))
+            FUV=(VF'./sum(VF)')*UV;
+            EUV=VE'*UV/2;
+            newUV=[UV;FUV;EUV];
+            clear FUV EUV;
+        end
+        
+        [I,J]=find(VF);
+        n=size(V,1);
+        m=size(F,1);
+        newF=nan(nnz(VF),4);
+        for k=1:nnz(VF)
+            i=I(k);
+            j=J(k);
+            newF(k,:)=[i n+m+find(and(and(VE(i,:),FE(j,:)),~xor(dirV(i,:),dirF(j,:)))) n+j n+m+find(and(and(VE(i,:),FE(j,:)),xor(dirV(i,:),dirF(j,:))))];
+        end
+        clear i I i I n m VE FE dirV dirF;
+        F=newF;
+        V=newV;
+        JW=newJW;
+        if(~isempty(UV))
+            UV=newUV;
+            clear newUV;
+        end
+        clear newF newV;
+    end
+    [J,W]=fromJW(JW);
+    if(isempty(UV))
+        varargout={J,W};
+    else
+        varargout={UV,J,W};
+    end
+end
+
+function JW=toJW(J,W)
+    % Convert given joints and weights matrices to a sparse weighted
+    % incidence matrix.
+    tempJ=reshape(J,size(J,1),[]);
+    tempW=reshape(W,size(W,1),[]);
+    present=~all(tempW==0,1);
+    tempW=tempW(:,present);
+    tempJ=tempJ(:,present);
+    I=repmat((1:size(tempJ,1))',1,nnz(present));
+    JW=sparse(I(:),tempJ(:)+1,tempW);
+end
+
+function [J,W]=fromJW(JW)
+    % Convert given sparse weighted incidence matrix to joints and weights
+    % format.
+    [J,I]=find(JW');
+    tempW=sparse(I,sum(cumsum(I==1:size(JW,1)).*(I==1:size(JW,1)),2),JW(sub2ind(size(JW),I,J)));
+    tempJ=sparse(I,sum(cumsum(I==1:size(JW,1)).*(I==1:size(JW,1)),2),J-1);
+    [W,I]=sort(tempW,2,"descend");
+    J=repmat((1:size(tempW,1))',1,size(tempW,2));
+    J=sub2ind(size(tempW),J,I);
+    J=tempJ(J);
+    W=reshape([W zeros(size(W,1),4-mod(size(W,2)-1,4)-1)],size(W,1),4,[]);
+    J=reshape([J zeros(size(J,1),4-mod(size(J,2)-1,4)-1)],size(J,1),4,[]);
+end
---
+++ writePLY.m
@@ -0,0 +1,348 @@
+function writePLY(gltf,filename,varargin)
+    % Write a Stanford PLY file.
+    %
+    % WRITEPLY(GLTF,FILENAME) writes GLTF to a Wavefront OBJ file specified
+    % by filename.
+    %
+    % WRITEPLY(GLTF,FILENAME,'nodes',NODES) writes the specified NODES in
+    % the GLTF to a Wavefront OBJ file specified by filename. Each of the
+    % specified nodes should be a mesh node.
+    %
+    % WRITEPLY(GLTF,FILENAME,'meshes',MESHES) writes the specified MESHES
+    % in the GLTF to a Wavefront OBJ file specified by filename. This input
+    % is ignored if NODES are specified.
+    %
+    % WRITEPLY(...,'materialFile',materialFile) also exports the materials
+    % in the specified nodes or meshes to the specified .mtl file, and adds
+    % a reference to it in the .obj file.
+    %
+    % © Copyright 2014-2024 Rohan Chabukswar.
+    %
+    % This file is part of MATLAB GLTF.
+    %
+    % MATLAB GLTF is free software: you can redistribute it and/or modify
+    % it under the terms of the GNU General Public License as published by
+    % the Free Software Foundation, either version 3 of the License, or (at
+    % your option) any later version.
+    %
+    % MATLAB GLTF is distributed in the hope that it will be useful, but
+    % WITHOUT ANY WARRANTY; without even the implied warranty of
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    % General Public License for more details.
+    %
+    % You should have received a copy of the GNU General Public License
+    % along with MATLAB GLTF. If not, see <https://www.gnu.org/licenses/>.
+    %
+    % Write a Wavefront OBJ file.
+    %
+    % WRITEOBJ(GLTF,FILENAME) writes GLTF to a Wavefront OBJ file specified
+    % by filename.
+    %
+    % WRITEOBJ(GLTF,FILENAME,'nodes',NODES) writes the specified NODES in
+    % the GLTF to a Wavefront OBJ file specified by filename. Each of the
+    % specified nodes should be a mesh node.
+    %
+    % WRITEOBJ(GLTF,FILENAME,'meshes',MESHES) writes the specified MESHES
+    % in the GLTF to a Wavefront OBJ file specified by filename. This input
+    % is ignored if NODES are specified.
+    %
+    % WRITEOBJ(...,'materialFile',materialFile) also exports the materials
+    % in the specified nodes or meshes to the specified .mtl file, and adds
+    % a reference to it in the .obj file.
+    %
+    % © Copyright 2014-2024 Rohan Chabukswar.
+    %
+    % This file is part of MATLAB GLTF.
+    %
+    % MATLAB GLTF is free software: you can redistribute it and/or modify
+    % it under the terms of the GNU General Public License as published by
+    % the Free Software Foundation, either version 3 of the License, or (at
+    % your option) any later version.
+    %
+    % MATLAB GLTF is distributed in the hope that it will be useful, but
+    % WITHOUT ANY WARRANTY; without even the implied warranty of
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    % General Public License for more details.
+    %
+    % You should have received a copy of the GNU General Public License
+    % along with MATLAB GLTF. If not, see <https://www.gnu.org/licenses/>.
+    %
+    [~,isMesh,isSkin]=gltf.nodeTree();
+    nodes=find(isMesh)';
+    ips=inputParser;
+    ips.addParameter('nodes',[],@(x)GLTF.validateInteger(x,nodes));
+    ips.addParameter('meshes',[],@(x)GLTF.validateInteger(x,0,numel(gltf.meshes)-1));
+    ips.addParameter('materialFile',missing,@isstring);
+    ips.parse(varargin{:});
+    parameters=ips.Results;
+    materialFile=parameters.materialFile;
+    nodes=parameters.nodes;
+    if(isempty(nodes))
+        meshes=parameters.meshes;
+    else
+        meshes=[];
+    end
+    if(and(isempty(nodes),isempty(meshes)))
+        [~,isMesh,isSkin]=gltf.nodeTree();
+        nodes=find(isMesh)';
+    end
+    nodeF=cell(0,1);
+    nodeV=cell(0,1);
+    nodeN=cell(0,1);
+    nodeUV=cell(0,1);
+    ct=0;
+    mode_str_values=["POINTS","LINES","LINE_LOOP","LINE_STRIP","TRIANGLES","TRIANGLE_STRIP","TRIANGLE_FAN"];
+    mode_num_values=[       0,      1,          2,           3,          4,               5,             6];
+    use_materials=true;
+    if(or(ismissing(materialFile),~isprop(gltf,'materials')))
+        use_materials=false;
+    end
+    if(use_materials)
+        used_materials=false(numel(gltf.materials),1);        
+        nodeM=cell(0,1);
+        matct=0;
+    end
+    if(~isempty(nodes))
+        for node=nodes
+            mesh_mat=gltf.getNodeTransformation(node-1);
+            mesh=gltf.nodes{node}.mesh;
+            np=numel(gltf.meshes{mesh+1}.primitives);
+            for primitive=1:np
+                V=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.attributes.POSITION);
+                if(~isempty(V))
+                    if(isfield(gltf.meshes{mesh+1}.primitives{primitive}.attributes,'NORMAL'))
+                        N=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.attributes.NORMAL);
+                    end
+                    if(isSkin(node))
+                        IBM=gltf.getAccessor(gltf.skins{gltf.nodes{node}.skin+1}.inverseBindMatrices);
+                        W=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.attributes.WEIGHTS_0);
+                        J=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.attributes.JOINTS_0);
+                        j=gltf.skins{gltf.nodes{node}.skin+1}.joints;
+                        if(iscell(j))
+                            j=cell2mat(j);
+                        end
+                        nodeT=nan(4,4,numel(j));
+                        for i=1:numel(j)
+                            nodeT(:,:,i)=gltf.getNodeTransformation(j(i));
+                        end
+                        TJ=pagemtimes(nodeT,IBM);
+                        V=permute(sum(W.*permute(pagemtimes(permute([V ones(size(V,1),1)],[3 2 4 1]),'none',reshape(TJ(1:3,:,J'+1),3,4,size(J,2),size(J,1)),'transpose'),[4 3 2 1]),2),[1 3 2]);
+                        if(isfield(gltf.meshes{mesh+1}.primitives{primitive}.attributes,'NORMAL'))
+                            N=permute(sum(W.*permute(pagemtimes(permute([N zeros(size(N,1),1)],[3 2 4 1]),'none',reshape(TJ(1:3,:,J'+1),3,4,size(J,2),size(J,1)),'transpose'),[4 3 2 1]),2),[1 3 2]);
+                        end
+                    end
+                    ct=ct+1;
+                    nodeV{ct}=[V ones(size(V,1),1)]*mesh_mat(1:3,:)';
+                    if(isfield(gltf.meshes{mesh+1}.primitives{primitive}.attributes,'NORMAL'))
+                        nodeN{ct}=N*mesh_mat(1:3,1:3)';
+                    else
+                        nodeN{ct}=nan(size(V,1),3);
+                    end
+                    if(isfield(gltf.meshes{mesh+1}.primitives{primitive},'mode'))
+                        mode_num=gltf.meshes{mesh+1}.primitives{primitive}.mode;
+                    else
+                        mode_num=4;
+                    end
+                    mode_str=mode_str_values(mode_num_values==mode_num);
+                    switch(mode_str)
+                        case "LINES"
+                            E=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.indices);
+                            nodeF{ct}=GLTF.fromLines(E);
+                        case "LINE_LOOP"
+                            E=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.indices);
+                            nodeF{ct}=GLTF.fromLineLoop(E);
+                        case "LINE_STRIP"
+                            E=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.indices);
+                            nodeF{ct}=GLTF.fromLineStrip(E);
+                        case "TRIANGLES"
+                            F=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.indices);
+                            nodeF{ct}=GLTF.fromTriangles(F);
+                        case "TRIANGLE_STRIP"
+                            F=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.indices);
+                            nodeF{ct}=GLTF.fromTriangleStrip(F);
+                        case "TRIANGLE_FAN"
+                            F=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.indices);
+                            nodeF{ct}=GLTF.fromTriangleFan(F);
+                        otherwise
+                            nodeF{ct}=[];
+                    end
+                    if(isfield(gltf.meshes{mesh+1}.primitives{primitive}.attributes,'TEXCOORD_0'))
+                        nodeUV{ct}=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.attributes.TEXCOORD_0);
+                    else
+                        nodeUV{ct}=nan(size(V,1),2);
+                    end
+                    if(use_materials)
+                        if(isfield(gltf.meshes{mesh+1}.primitives{primitive},'material'))
+                            mat=gltf.meshes{mesh+1}.primitives{primitive}.material;
+                            used_materials(mat+1)=true;
+                            if(isfield(gltf.materials{mat+1},'name'))
+                                nodeM{ct}=gltf.materials{mat+1}.name;
+                            else
+                                matct=matct+1;
+                                nodeM{ct}="material_"+matct;
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    elseif(~isempty(meshes))
+        for mesh=meshes
+            np=numel(gltf.meshes{mesh+1}.primitives);
+            for primitive=1:np
+                V=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.attributes.POSITION);
+                if(~isempty(V))
+                    ct=ct+1;
+                    nodeV{ct}=V;
+                    if(isfield(gltf.meshes{mesh+1}.primitives{primitive},'mode'))
+                        mode_num=gltf.meshes{mesh+1}.primitives{primitive}.mode;
+                    else
+                        mode_num=4;
+                    end
+                    mode_str=mode_str_values(mode_num_values==mode_num);
+                    switch(mode_str)
+                        case "LINES"
+                            E=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.indices);
+                            nodeF{ct}=GLTF.fromLines(E);
+                        case "LINE_LOOP"
+                            E=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.indices);
+                            nodeF{ct}=GLTF.fromLineLoop(E);
+                        case "LINE_STRIP"
+                            E=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.indices);
+                            nodeF{ct}=GLTF.fromLineStrip(E);
+                        case "TRIANGLES"
+                            F=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.indices);
+                            nodeF{ct}=GLTF.fromTriangles(F);
+                        case "TRIANGLE_STRIP"
+                            F=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.indices);
+                            nodeF{ct}=GLTF.fromTriangleStrip(F);
+                        case "TRIANGLE_FAN"
+                            F=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.indices);
+                            nodeF{ct}=GLTF.fromTriangleFan(F);
+                        otherwise
+                            nodeF{ct}=[];
+                    end
+                    if(isfield(gltf.meshes{mesh+1}.primitives{primitive}.attributes,'NORMAL'))
+                        N=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.attributes.NORMAL);
+                        nodeN{ct}=N;
+                    else
+                        nodeN{ct}=nan(size(V,1),3);
+                    end
+                    if(isfield(gltf.meshes{mesh+1}.primitives{primitive}.attributes,'TEXCOORD_0'))
+                        nodeUV{ct}=gltf.getAccessor(gltf.meshes{mesh+1}.primitives{primitive}.attributes.TEXCOORD_0);
+                    else
+                        nodeUV{ct}=nan(size(V,1),2);
+                    end
+                    if(use_materials)
+                        if(isfield(gltf.meshes{mesh+1}.primitives{primitive},'material'))
+                            mat=gltf.meshes{mesh+1}.primitives{primitive}.material;
+                            used_materials(mat+1)=true;
+                            if(isfield(gltf.materials{mat+1},'name'))
+                                nodeM{ct}=gltf.materials{mat+1}.name;
+                            else
+                                matct=matct+1;
+                                nodeM{ct}="material_"+matct;
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+
+    nodeE=nodeF;
+    edges=cellfun(@(x)size(x,2)==2,nodeE);
+    faces=cellfun(@(x)size(x,2)==3,nodeF);
+    nodeE(~edges)=repmat({uint32(zeros(0,2))},size(nodeE(~edges)));
+    nodeF(~faces)=repmat({uint32(zeros(0,3))},size(nodeF(~faces)));
+
+    nonormals=cellfun(@(x)all(isnan(x),"all"),nodeN);
+    if(~or(all(nonormals),all(~nonormals)))
+        I=find(nonormals);
+        for i=1:numel(I)
+            if(edges(I(i)))
+                N=zeros(size(nodeV{I(i)},1),3);
+                % nodeN{I(i)}=N./vecnorm(N,2,2);
+            else
+                [N,F,idx]=GLTF.vertexNormals(nodeF{I(i)},nodeV{I(i)});
+                nodeF{I(i)}=F;
+                nodeV{I(i)}=nodeV{I(i)}(idx,:);
+                nodeN{I(i)}=N;
+            end
+        end
+    end
+
+    F=cellfun(@(x,y)x+y,nodeF(:),num2cell([0;cumsum(cellfun(@(x)size(x,1),nodeV(1:end-1)'))]),"UniformOutput",false);
+    E=cellfun(@(x,y)x+y,nodeE(:),num2cell([0;cumsum(cellfun(@(x)size(x,1),nodeV(1:end-1)'))]),"UniformOutput",false);
+    V=cell2mat(nodeV(:));
+    N=cell2mat(nodeN(:));
+    UV=cell2mat(nodeUV(:));
+    F=cell2mat(F);
+    E=cell2mat(E);
+
+    if(and(~all(isnan(UV),"all"),~all(isnan(N),"all")))
+        VUVN=[V UV N];
+    elseif(~all(isnan(UV),"all"))
+        VUVN=[V UV];
+    elseif(~all(isnan(N),"all"))
+        VUVN=[V N];
+    else
+        VUVN=V;
+    end
+
+    header=[
+        "ply"
+        "format ascii 1.0"
+        ];
+
+    vertex_header=[
+        "element vertex "+string(size(V,1))
+        "property float x"
+        "property float y"
+        "property float z"
+        ];
+    if(~all(isnan(UV),"all"))
+        vertex_header=[
+            vertex_header
+            "property float s"
+            "property float t"
+            ];
+        if(~all(isnan(N),"all"))
+            vertex_header=[
+                vertex_header
+                "property float nx"
+                "property float ny"
+                "property float nz"
+                ];
+        end
+    elseif(~all(isnan(N),"all"))
+        vertex_header=[
+            vertex_header
+            "property float nx"
+            "property float ny"
+            "property float nz"
+            ];
+    end
+    clear ic;
+    faces_header=[
+        "element face "+string(size(F,1))
+        "property list uchar uint vertex_indices"
+        ];
+    edges_header=[
+        "element edge "+string(size(E,1))
+        "property int vertex1"
+        "property int vertex2"
+        ];
+    end_header="end_header";
+
+    vtext=string(VUVN);
+    vtext(ismissing(vtext))="";
+    vtext=strip(join(vtext));
+    ftext=string([sum(~isnan(F),2) F-1]);
+    ftext(ismissing(ftext))="";
+    ftext=strip(join(ftext));
+    etext=string(E-1);
+    etext=strip(join(etext));
+    writelines([header;vertex_header;faces_header;edges_header;end_header;vtext;ftext;etext],filename);
+    clear vtext ftext header vertex_header faces_header end_header;
+end
